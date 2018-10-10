b4c1a5d97981d87f37e85c1895a8d3ed5f7f39c0
LavenderTr <42264396+LavenderTr@users.noreply.github.com>
2018-10-11 00:22:46 +0800

    This file contains main data structs .

---
+++ data.h
@@ -0,0 +1,14 @@
+#ifndef DATA_H
+#define DATA_H
+
+typedef struct tag{
+  int moclecular,
+  int denominator
+  } fraction;
+typedef struct tag{
+  fraction matr[][],
+  int m,
+  int n
+  } M;
+  
+#endif
